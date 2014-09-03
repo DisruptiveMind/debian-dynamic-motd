259f5857b5972d679d043e6ac646731de0d0fc8f
Tyler Pham <tyler@disruptiveart.com>
2014-09-02 23:02:19 -0400

    added download instructions to get zip

--- install.sh
+++ install.sh
@@ -4,13 +4,19 @@
 ## Script taken from
 #####
 
-FOLDER="debian-dynamic-motd"
+PROJECT="debian-dynamic-motd"
+BRANCH="master"
+URL="https://github.com/DisruptiveMind/$PROJECT"
+FOLDER=$PROJECT
 
 # install figlet to enable ASCII art
 sudo apt-get install figlet
 
 # create directory
 mkdir /etc/update-motd.d/
+
+# create the paths
+wget "$URL/archive/$BRANCH.zip" ; unzip "$BRANCH.zip"
 
 # copy all files into place
 cd $FOLDER ; mv motd/* /etc/update-motd.d/
